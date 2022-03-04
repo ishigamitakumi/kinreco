788b0121081f11cb9c8ad19786bf4a4f62f0e60e
EC2 Default User <ec2-user@ip-172-31-93-123.ec2.internal>
2022-03-04 06:56:04 +0000

    feat: homesコントローラ追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+root :to => 'homes#top'
+get "homes/about" => "homes#about"
+
+scope module: :public do
+
+end
+
+namespace :admin do
+
+end
 end
